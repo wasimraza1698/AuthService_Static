875d8af857c0ff7fb571a89a1c952842cc69aed6
Wasim Raza <wasimraza1698@gmail.com>
2020-11-26 14:54:46 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthService.Models;
+using AuthService.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Controllers
+{
+    public class UserController : Controller
+    {
+        private static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(UserController));
+        private readonly IConfiguration _config;
+        public readonly IUserRepo _userRepo;
+        public UserController(IUserRepo userRepo,IConfiguration config)
+        {
+            _userRepo = userRepo;
+            _config = config;
+        }
+        [HttpPost("Get")]
+        public User Get([FromBody]User valUser)
+        {
+            _log4net.Info("Trying to get User");
+            return _userRepo.Get(valUser);
+        }
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody]User user)
+        {
+
+            try
+            {
+                _log4net.Info("Authentication initiated for UserId " + user.UserID.ToString());
+                IActionResult response;
+                User valUser = _userRepo.Get(user);
+                if (valUser == null)
+                {
+                    _log4net.Info("User not found");
+                    return NotFound();
+                }
+                else
+                {
+                    _log4net.Info("Logging in user with id " + valUser.UserID.ToString());
+                    var tokenString = GenerateJSONWebToken(valUser); response = Ok(new { token = tokenString });
+                    return response;
+                }
+            }
+            catch
+            {
+                return new NoContentResult();
+            }
+        }
+        private string GenerateJSONWebToken(User user)
+        {
+            _log4net.Info("Token Generation initiated for UserId " + user.UserID.ToString());
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jet:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token).ToString();
+        }
+    }
+}
---
+++ Repositories/UserRepo.cs
@@ -0,0 +1,26 @@
+﻿using AuthService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Repositories
+{
+    public class UserRepo : IUserRepo
+    {
+        List<User> users = new List<User>()
+        {
+            new User(){UserID = 1,UserName = "wasim1698",Password = "123456"},
+            new User(){UserID = 2,UserName = "Raza",Password = "123456"},
+            new User(){UserID = 3,UserName = "Mohammad",Password = "123456"},
+            new User(){UserID = 4,UserName = "LM",Password = "123456"},
+            new User(){UserID = 5,UserName = "KDB",Password = "123456"},
+            new User(){UserID = 6,UserName = "Xavi",Password = "123456"},
+            new User(){UserID = 7,UserName = "Admin",Password = "Admin@123"}
+        };
+        public User Get(User user)
+        {
+            return users.SingleOrDefault(u => u.UserName == user.UserName && u.Password == user.Password);
+        }
+    }
+}
