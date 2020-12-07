724ab0e56ed9f88d57ed5b3ff79b2f5c6eca3593
wasimraza1698 <wasimraza1698@gmail.com>
2020-12-07 12:17:17 +0530

    Index Added

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,8 +27,10 @@ namespace AuthService.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            var i=1;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Index = i++;
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
